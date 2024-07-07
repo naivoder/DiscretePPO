07bca54b36e6ed475aea389a8253d75b9191e021
credovian <cameron.redovian@r-dex.com>
2024-07-07 16:08:15 -0400

    add clip reward and action adapter utils

--- utils.py
+++ utils.py
@@ -4,6 +4,20 @@ import imageio
 import cv2
 
 
+def clip_reward(reward):
+    if reward < -1:
+        return -1
+    elif reward > 1:
+        return 1
+    else:
+        return reward
+
+
+# https://github.com/XinJingHao/PPO-Continuous-Pytorch
+def action_adapter(a, max_a):
+    return 2 * (a - 0.5) * max_a
+
+
 def preprocess_frame(frame):
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
     frame = cv2.resize(frame, (84, 84))
