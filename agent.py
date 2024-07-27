fixing animate file, add dim arg to softmax

agent.py
@@ -42,7 +42,7 @@ class ActorCritic(torch.nn.Module):
         x = x.view(x.size()[0], -1)
         x = torch.nn.functional.relu(self.fc1(x))
         value = self.critic(x)
-        x = torch.nn.functional.softmax(self.actor(x))
+        x = torch.nn.functional.softmax(self.actor(x), dim=1)
         action = torch.distributions.Categorical(logits=x)
         return action, value
 

animate.py
@@ -5,25 +5,24 @@ from argparse import ArgumentParser
 from utils import save_animation
 
 def generate_animation(env_name):
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     env = AtariEnv(
             env_name,
             shape=(84, 84),
             repeat=4,
-            clip_rewards=True,
+            clip_rewards=False,
             no_ops=0,
             fire_first=False,
         ).make()
+    
     agent = DiscretePPOAgent(
         env_name,
         env.observation_space.shape,
         env.action_space.n)
     
     # agent.load_checkpoints()
-    agent.actor.load_state_dict(torch.load(f"weights/{env_name}_actor_final.pt"))
-    agent.critic.load_state_dict(torch.load(f"weights/{env_name}_critic_final.pt"))
+    agent.network.load_state_dict(torch.load(f"weights/{env_name}_final.pt"))
 
-    best_total_reward = float("-inf")
+    best_total_reward = min(env.reward_range)
     best_frames = None
 
     for _ in range(10):
@@ -35,7 +34,7 @@ def generate_animation(env_name):
         while not term and not trunc:
             frames.append(env.render())
             with torch.no_grad():
-                action, _, _, _ = agent.choose_action(state)
+                action, _, _ = agent.choose_action(state)
             next_state, reward, term, trunc, _ = env.step(action)
             state = next_state
             total_reward += reward

main.py
@@ -53,8 +53,7 @@ def run_ppo(args):
 
     states, _ = envs.reset()
     while len(history) < args.n_games:
-        with torch.no_grad():
-            apvs = [agent.choose_action(state) for state in states]
+        apvs = [agent.choose_action(state) for state in states]
         actions, probs, values = list(map(list, zip(*apvs)))
 
         next_states, rewards, term, trunc, _ = envs.step(actions)